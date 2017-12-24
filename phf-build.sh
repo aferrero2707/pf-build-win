2b69332a5defaa66be753323b441b17845cf1934
aferrero2707 <aferrero1975@gmail.com>
2017-12-24 15:30:04 +0100

    Changed compile mode to Release

--- phf-build.sh
+++ phf-build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 echo "Compiling photoflow"
-(crossroad cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUNDLED_LENSFUN=ON /sources && make -j 2 && make install) || exit 1
+(crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLED_LENSFUN=ON /sources && make -j 2 && make install) || exit 1
